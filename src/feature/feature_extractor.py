8019a0df28286772b463099b8ff9af63f7c28a9d
Ajin Tom <ajinjtom@gmail.com>
2018-09-29 16:42:36 -0400

    read train data into h5f

--- src/feature/feature_extractor.py
+++ src/feature/feature_extractor.py
@@ -9,9 +9,19 @@ import librosa
 import librosa.display
 #https://musicinformationretrieval.com/ipython_audio.html
 
-# implement iterator to load wav files into x
-# feed traindata.wav into wavfile
 
-wavfile = ".wav"
-x, sr = librosa.load(wavfile, sr = 16000, mono = 'True')
-C = np.abs(librosa.cqt(x[:int(2.68*sr)], sr=sr, n_bins=84))
+# implement iterator to load wav files into x
+
+import os,glob,h5py
+cqarray = np.zeros((1,84,84))
+os.chdir(os.getcwd())
+filenames = glob.glob("*.wav")
+# print (filenames)
+for i in range(len(filenames)):
+    x,sr = librosa.load(filenames[i], sr = 16000, mono = 'True')
+    c = np.abs(librosa.cqt(x[:int(2.68*sr)], sr=sr, n_bins=84))
+    c = np.reshape(c,(1,84,84))
+    cqarray = np.append(cqarray,c,0)
+
+h5f = h5py.File('train_data.h5','w')
+h5f.create_dataset('train',data = cqarray)
